38bbdc6eff12b4fd04a852e8f7e5faeeb3a3f036
Christopher Burger <burger.christopher@hotmail.ca>
2019-07-13 19:57:26 -0400

    retrieve number range matches for playerName and save to data-store

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const bodyParser = require('body-parser');
 const pino = require('express-pino-logger')();
 const JSON = require('circular-json');
 require('dotenv').config()
-
+//nodemon --ignore config.json
 const axios = require("axios");
 
 const Store = require('data-store');
@@ -33,8 +33,8 @@ app.get('/seasons', async (req, res) => {
 
 app.get('/player', async (req, res) => {
   try {
-    if(store.get('playerData-${req.query.playerName}') != null) {
-      res.send(store.get('playerData-${req.query.playerName}'))
+    if(store.get(`playerData-${req.query.playerName}`) != null) {
+      res.send(store.get(`playerData-${req.query.playerName}`))
     } else {
       const response = await axios.get(`https://api.pubg.com/shards/steam/players?filter[playerNames]=${req.query.playerName}`,{
         headers: {
@@ -45,10 +45,44 @@ app.get('/player', async (req, res) => {
       });
       let cleanResponse = response.data.data[0]
       let matchIds = cleanResponse
-      store.set('playerData-${req.query.playerName}', {accountId: cleanResponse.id, ...cleanResponse.attributes, matches: cleanResponse.relationships.matches.data})
+      store.set(`playerData-${req.query.playerName}`, {accountId: cleanResponse.id, ...cleanResponse.attributes, matches: cleanResponse.relationships.matches.data})
       res.setHeader('Content-Type', 'application/vnd.api+json');
       res.send(store.get('playerData-${req.query.playerName}')); 
     }
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+app.get('/matches', async (req, res) => {
+  try {
+    let storeData = store.get(`playerData-${req.query.playerName}`)
+    let rangeStart = req.query.rangeStart
+    let rangeEnd = req.query.rangeEnd
+    let query, matchId, response, matches = []
+    let accountId = store.get(`playerData-${req.query.playerName}`)
+    for(let i = rangeStart; i <= rangeEnd; i++) {
+      matchId = storeData.matches[i].id
+      query =  `https://api.pubg.com/shards/steam/matches/${matchId}`
+      response = await axios.get(query,{
+        headers: {
+          Accept: "application/vnd.api+json",
+        }
+      })
+      matches.push(response.data)
+    }
+    matchesReduced = matches.map(match => 
+      ({
+        attributes: match.data.attributes, 
+        stats: match.included.map(players => 
+          players.type == 'participant' && players
+        ).filter(player => player && player.attributes.stats.name == req.query.playerName)
+      })
+    )
+    store.set(`playerData-${req.query.playerName}`, {...storeData, matchDataArray: matchesReduced})
+    res.setHeader('Content-Type', 'application/vnd.api+json');
+    res.send(matchesReduced)
+    
   } catch (error) {
     console.log(error);
   }
