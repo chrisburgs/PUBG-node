8bb5cb4736b34dc2a27f2013619b22439f7904a5
Christopher Burger <burger.christopher@hotmail.ca>
2019-07-14 20:49:32 -0400

    add the telemetry url to player match data don't store all match data as it's long and doesn't require api key anyway

--- index.js
+++ index.js
@@ -38,17 +38,12 @@ app.get("/player", async (req, res) => {
 		if (store.get(`playerData-${req.query.playerName}`) != null) {
 			res.send(store.get(`playerData-${req.query.playerName}`))
 		} else {
-			const response = await axios.get(
-			`https://api.pubg.com/shards/steam/players?filter[playerNames]=${
-				req.query.playerName
-			}`,
-			{
+			const response = await axios.get(`https://api.pubg.com/shards/steam/players?filter[playerNames]=${req.query.playerName}`, {
 				headers: {
-				Accept: "application/vnd.api+json",
-				Authorization: `Bearer ${process.env.apikey}`
+					Accept: "application/vnd.api+json",
+					Authorization: `Bearer ${process.env.apikey}`
 				}
-			}
-			)
+			})
 			let cleanResponse = response.data.data[0]
 			let matchIds = cleanResponse
 			store.set(`playerData-${req.query.playerName}`, {
@@ -80,8 +75,8 @@ app.get("/matches", async (req, res) => {
 			}
 		})
 		matches.push({...response.data, matchId: matchId})
-		store.union('matchData', response.data.data)
 	}
+	// console.log('matches', JSON.stringify(matches))
 	matchesReduced = matches.map(match => ({
 		attributes: match.data.attributes,
 		matchId: matchId,
@@ -90,7 +85,8 @@ app.get("/matches", async (req, res) => {
 		.filter(
 			player =>
 			player && player.attributes.stats.name == req.query.playerName
-		)
+		),
+		asset: match.included.filter(inc => inc.type == 'asset')
 	}))
 	store.set(`playerData-${req.query.playerName}`, {
 		...storeData,
@@ -106,30 +102,63 @@ app.get("/matches", async (req, res) => {
 app.get('/kd', async (req, res) => {
 	try {
 		let storeData = store.get(`playerData-${req.query.playerName}`)
-		// let query = `https://api.pubg.com/shards/steam/matches/${req.query.matchID}`
-		query = `https://api.pubg.com/shards/steam/matches/751c0d02-66f6-4aef-bc5f-55259e3bb1aa`
-		response = await axios.get(query, {
-			headers: {
-				Accept: "application/vnd.api+json"
-			}
-		})
-		// console.log('response', JSON.stringify(response.data.data))
-		let playerList = response.data.data.map(player => console.log('player', player))
-		// let response = axios.get(`https://api.pubg.com/shards/steam/seasons/division.bro.official.pc-2018-03/gameMode/squad-fpp/players?filter[playerIds]=${query}`, {
-		// 	headers: {
-		// 		Accept: "application/vnd.api+json"
-		// 	}
-		// })
+		let accountList = await getAccountList(req.query.matchId)
+		let playerData = [], data = []
+		let response
+		for(query of accountList) {
+			response = await axios.get(`https://api.pubg.com/shards/steam/seasons/${req.query.season}/gameMode/${req.query.gameMode}/players?filter[playerIds]=${query}`, {
+				headers: {
+					Accept: "application/vnd.api+json",
+					Authorization: `Bearer ${process.env.apikey}`
+				},
+				validateStatus: function (status) {
+					return status >= 200 && status <= 300 || status == 429; // default
+				}
+			})
+			response.status != 429 ? data.push(response.data.data) : null
+			
+		}
+		data = [].concat(...data);
+		data = data.map(data => data.attributes.gameModeStats[req.query.gameMode]);
+
+		res.setHeader("Content-Type", "application/vnd.api+json")
+		res.send(data)
 	} catch (error) {
 		console.log(error)
 	}
 })
 
+
+async function getAccountList(matchId) {
+	let splicedList = []
+	query = `https://api.pubg.com/shards/steam/matches/5f4807fa-562c-4a09-854c-b6b9aa473622`
+	response = await axios.get(query, {
+		headers: {
+			Accept: "application/vnd.api+json"
+		}
+	})
+	let accountList = response.data.included.filter(inc => inc.type == 'participant')
+	accountList = accountList.map(player => player.attributes.stats.playerId)
+	while (accountList.length) {
+		splicedList.push(accountList.splice(0,10).join('%2C'))
+	}
+	return splicedList
+}
 app.get("/clear", (req, res) => {
 	store.clear()
 	res.send(JSON.stringify({ ...store.data }))
 })
 
+
+app.get('/telemetryTest', async (req, res) => {
+	try {
+		let response = axios.get({
+
+		})
+	} catch (error) {
+		console.log(error)
+	}
+})
 app.listen(3001, () =>
 	console.log("Express server is running on localhost:3001")
 )
